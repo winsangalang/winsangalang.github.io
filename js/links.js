f20855a8d290bd45f93d1d20fc1bb72da2e16752
sherwin <winsan11387@gmail.com>
2025-03-24 09:28:10 +0800

    Update links.js

--- js/links.js
+++ js/links.js
@@ -1,20 +1,21 @@
 const itemList = [
-    "<a href="https://www.casinoplus.com.ph/home" target="_blank">Casinoplus.com.ph</a>",
-    "<a href="https://www.okbet.com/mobile/login" target="_blank">Okbet</a>",
-    "<a href="https://www.playtime.ph/" target="_blank">Playtime</a>",
-    "<a href="https://bingoplus.com/" target="_blank">Bingoplus</a>",
-    "<a href="https://arenaplus.ph/" target="_blank">Arenaplus</a>",
-    "<a href="https://gzone.ph/#login" target="_blank">Gamezone</a>"
-];
+            '<a href="https://www.casinoplus.com.ph/home" target="_blank">Casinoplus.com.ph</a>',
+            '<a href="https://www.okbet.com/mobile/login" target="_blank">Okbet</a>',
+            '<a href="https://www.playtime.ph/" target="_blank">Playtime</a>',
+            '<a href="https://bingoplus.com/" target="_blank">Bingoplus</a>',
+            '<a href="https://arenaplus.ph/" target="_blank">Arenaplus</a>',
+            '<a href="https://gzone.ph/#login" target="_blank">Gamezone</a>'
+        ];
 
-function renderList() {
-    const ul = document.getElementById("dynamicList");
-    ul.innerHTML = "";
-    itemList.forEach(item => {
-        const li = document.createElement("li");
-        li.textContent = item;
-        ul.appendChild(li);
-    });
-}
+        function renderList() {
+            const ul = document.getElementById("dynamicList");
+            ul.innerHTML = "";
+            itemList.forEach(item => {
+                const li = document.createElement("li");
+                li.classList.add("custom-li"); // Add class to prevent affecting other lists
+                li.innerHTML = item; // Properly render anchor tags
+                ul.appendChild(li);
+            });
+        }
 
-document.addEventListener("DOMContentLoaded", renderList);
+        document.addEventListener("DOMContentLoaded", renderList);
